db7af67fd20a200e8f86d96da04a926b9bbeac45
ngactiensinh <ngacvantuan.hg@gmail.com>
2026-05-02 20:50:59 +0700

    Add time module import to dang_ky_so.py

--- dang_ky_so.py
+++ dang_ky_so.py
@@ -3,6 +3,7 @@ import pandas as pd
 import base64
 from datetime import datetime
 import pytz
+import time
 from supabase import create_client, Client
 import streamlit.components.v1 as components
 
